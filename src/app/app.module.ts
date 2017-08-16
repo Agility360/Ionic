1a90d56e9c9eb2bb72f811e2b914692285e53fc4
Lawrence McDaniel <lpm0073@gmail.com>
2017-08-16 15:25:52 -0500

    deleted AWS starter app 'About Page'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { LoginPage } from '../pages/login/login';
 import { SignupPage } from '../pages/signup/signup';
 import { ConfirmPage } from '../pages/confirm/confirm';
 import { SettingsPage } from '../pages/settings/settings';
-import { AboutPage } from '../pages/about/about';
 import { AccountPage } from '../pages/account/account';
 import { TabsPage } from '../pages/tabs/tabs';
 import { TasksPage } from '../pages/tasks/tasks';
@@ -17,6 +16,7 @@ import { TasksCreatePage } from '../pages/tasks-create/tasks-create';
 import { JobsPage } from '../pages/jobs/jobs';
 import { NewsPage } from '../pages/news/news';
 import { ForumPage } from '../pages/forum/forum';
+import { NotificationsPage } from '../pages/notifications/notifications';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -33,14 +33,14 @@ import { DynamoDB } from '../providers/aws.dynamodb';
     SignupPage,
     ConfirmPage,
     SettingsPage,
-    AboutPage,
     AccountPage,
     TabsPage,
     TasksPage,
     TasksCreatePage,
     JobsPage,
     NewsPage,
-    ForumPage
+    ForumPage,
+    NotificationsPage
   ],
   imports: [
     BrowserModule,
@@ -53,14 +53,14 @@ import { DynamoDB } from '../providers/aws.dynamodb';
     SignupPage,
     ConfirmPage,
     SettingsPage,
-    AboutPage,
     AccountPage,
     TabsPage,
     TasksPage,
     TasksCreatePage,
     JobsPage,
     NewsPage,
-    ForumPage
+    ForumPage,
+    NotificationsPage
   ],
   providers: [
     StatusBar,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -5,6 +5,7 @@ import { TasksPage } from '../tasks/tasks';
 import { JobsPage } from '../jobs/jobs';
 import { NewsPage } from '../news/news';
 import { ForumPage } from '../forum/forum';
+import { NotificationsPage } from '../notifications/notifications';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -14,7 +15,7 @@ export class TabsPage {
   tab1Root = JobsPage;
   tab2Root = NewsPage;
   tab3Root = ForumPage;
-  tab4Root = TasksPage;
+  tab4Root = NotificationsPage;
   tab5Root = SettingsPage;
 
   constructor() {
