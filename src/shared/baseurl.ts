cea2c8eef20ece4fdd03e0dbefe1f5c891819366
Lawrence McDaniel <lpm0073@gmail.com>
2017-08-21 09:51:07 -0500

    modified cms base URL

--- src/shared/baseurl.ts
+++ src/shared/baseurl.ts
@@ -2,5 +2,5 @@
 /* https://api.agility360app.net/beta/ */
 export const baseURL = 'https://hqctqkd7xc.execute-api.us-east-1.amazonaws.com/beta/'
 export const cdnURL = 'https://cdn.agility360app.net/'
-export const cmsURL = 'http://cms.agility360app.net/wp-json/wp/v2/'
+export const cmsURL = 'http://agility360app.net/wp-json/wp/v2/'
 export const DEBUG_MODE = false
