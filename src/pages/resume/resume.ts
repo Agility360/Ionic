9b59da5b5300a399c8a762bb5597ade94aceda60
Lawrence McDaniel <lpm0073@gmail.com>
2017-08-29 10:10:27 -0500

    added wordpress feed to Resume Tips page

--- src/pages/resume/resume.ts
+++ src/pages/resume/resume.ts
@@ -1,22 +1,94 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ForumPage page.
+/*------------------------------------------------------
+ * written by: mcdaniel
+ * date: august 2017
  *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+ * usage: template controller fed by custom Wordpress provider
+ *        this code is templated for use in the following
+ *        page controllers:
+ *        - jobs
+ *        - news
+ *        - resume
+ *------------------------------------------------------*/
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DEBUG_MODE } from '../../shared/constants';
+import { WPPost } from '../../shared/wppost';
+import { WordpressProvider } from '../../providers/wordpress';
 
 @IonicPage()
 @Component({
   selector: 'page-resume',
   templateUrl: 'resume.html',
 })
+
 export class ResumePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  posts: WPPost[];
+  errMess: string;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private wpservice: WordpressProvider
+              ) {
+        if (DEBUG_MODE) console.log('constructor ResumePage');
+        this.getPosts();
   }
 
+  refreshData(refresher) {
+      setTimeout(() => {
+        if (DEBUG_MODE) console.log('ResumePage.refreshData()');
+        this.getPosts();
+        refresher.complete();
+      }, 500);
+  }
+
+  getPosts() {
+    if (DEBUG_MODE) console.log('ResumePage.getPosts()');
+
+    /* Note: set this to appropriate parameter factory fuction:
+        paramsJobs()
+        paramsNews()
+        paramsResume()
+    */
+    let params = this.wpservice.paramsNews();
+
+    this.wpservice.getPosts(params)
+      .subscribe(
+        results => {
+          if (DEBUG_MODE) console.log('ResumePage.getPosts() - success', results);
+          this.posts = results
+          var self = this;
+          this.posts.forEach(function(post, id){
+              if (DEBUG_MODE) console.log(post);
+              self.getMedia(post);
+          });
+        },
+        err => {
+          if (DEBUG_MODE) console.log('ResumePage.getPosts() - error', err);
+          this.errMess = <any>err
+        });
+  }
+
+  getMedia(post: WPPost) {
+
+    if (DEBUG_MODE) console.log('ResumePage.getMedia()', post);
+    if (post.featured_media == 0) {
+      post.featured_media_obj = this.wpservice.newMedia();
+      return;
+    };
+
+    this.wpservice.getMedia(post.featured_media)
+      .subscribe(
+        results => {
+          if (DEBUG_MODE) console.log('ResumePage.getMedia() - success', results);
+          post.featured_media_obj = results;
+          post.featured_media_url = results.source_url;
+        },
+        err => {
+          if (DEBUG_MODE) console.log('ResumePage.getMedia() - error', err);
+          this.errMess = <any>err;
+        });
+
+  }
 
 }
