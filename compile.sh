d7417218af8c43d0016fba55ea75fcc5393015a1
Lawrence McDaniel <lpm0073@gmail.com>
2017-09-17 11:04:02 -0500

    relocated image assets

--- compile.sh
+++ compile.sh
@@ -7,6 +7,7 @@ aws s3 cp --recursive ./www s3://clientengagementapp-hosting-mobilehub-136394481
 echo "Web app compiled."
 echo "To execute from S3: http://clientengagementapp-hosting-mobilehub-1363944817.s3-website-us-east-1.amazonaws.com/"
 echo "To execute from Cloudfront: https://d1s3rwkbgrvc9r.cloudfront.net/"
+echo "To execute in prod: https://web.agility360app.net"
 
 echo "Building Ionic View app."
 ionic upload
