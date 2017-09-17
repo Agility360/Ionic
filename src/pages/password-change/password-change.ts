ce9671578844baf2e6666a25f79136a7b52efbe5
Lawrence McDaniel <lpm0073@gmail.com>
2017-09-17 09:43:10 -0500

    added form field validations

--- src/pages/password-change/password-change.ts
+++ src/pages/password-change/password-change.ts
@@ -15,7 +15,6 @@ import { DEBUG_MODE } from '../../shared/constants';
 export class PasswordChangePage {
 
   public formGroup: FormGroup;
-  public errMsg: any;
   public currentPassword: string;
   public newPassword: string;
   public ReenterNewPassword: string;
@@ -42,11 +41,11 @@ export class PasswordChangePage {
       ],
       'newPassword': [
         '',
-        Validators.compose([Validators.required, Validators.minLength(8)])
+        Validators.compose([Validators.required])
       ],
       'ReenterNewPassword': [
         '',
-        Validators.compose([Validators.required, this.validateRepeatPassword.bind(this)])
+        Validators.compose([Validators.required])
       ]
     });
 
@@ -55,6 +54,8 @@ export class PasswordChangePage {
 
   changePassword() {
     if (DEBUG_MODE) console.log('PasswordChangePage.changePassword()');
+
+    if (!this.formValidate()) return;
 
     this.cognitoUser.changePassword(this.currentPassword, this.newPassword, function(err, result) {
       if (err) {
@@ -75,18 +76,6 @@ export class PasswordChangePage {
 
   }
 
-  validateRepeatPassword(control: AbstractControl): { [key: string]: any } {
-    if (DEBUG_MODE) console.log('PasswordChangePage.validateRepeatPassword()');
-    return null;
-
-    /*
-        if (control.value != this.newPassword) {
-          return null;
-        } else {
-          return { validateRepeatPassword: true };
-        }
-    */
-  }
 
   ionViewDidLoad() {
     if (DEBUG_MODE) console.log('PasswordChangePage.ionViewDidLoad()');
@@ -96,41 +85,43 @@ export class PasswordChangePage {
   formValidate(): boolean {
     if (DEBUG_MODE) console.log('PasswordChangePage.formValidate()');
 
-    if (this.formGroup.valid) {
-      return true;
-    }
-
     // figure out the error message
-    let errorMsg = '';
+    let errorMsg: string = null;
 
     // validate password ...
     let control = this.formGroup.controls['newPassword'];
     if (!control.valid) {
       if (control.errors['required']) {
         errorMsg = 'Please provide a new password';
-      } else if (control.errors['minlength']) {
-        errorMsg = 'The new password should be at least 5 characters';
       }
     }
-
 
     // validate repeat password ...
     control = this.formGroup.controls['ReenterNewPassword'];
     if (!control.valid) {
       if (control.errors['required']) {
         errorMsg = 'Please re-enter the new password';
-      } else if (control.errors['validateRepeatPassword']) {
-        errorMsg = 'Passwords do not match';
       }
     }
 
-    let alert = this.alertCtrl.create({
-      //title: 'Error!',
-      subTitle: errorMsg || 'Empty error message!',
-      buttons: ['OK']
-    });
+    if (errorMsg == null && this.newPassword != this.ReenterNewPassword) {
+      errorMsg = 'Passwords do not match';
+    }
 
-    alert.present(alert);
+    if (errorMsg != null) {
+      let alert = this.alertCtrl.create({
+        //title: 'Error!',
+        subTitle: errorMsg || 'Empty error message!',
+        buttons: ['OK']
+      });
+
+      alert.present(alert);
+      return false;
+    }
+
+    if (this.formGroup.valid) {
+      return true;
+    }
 
     return false;
   }
